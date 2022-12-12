ce2b7534f04cc72c07afda9ad63dd785e92f2e73
Miguel Martinez <miguel.emezero@gmail.com>
2022-12-12 15:04:24 -0600

    add outlet for every route change, and add new sidebar

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,9 +1,11 @@
 import Sidebar from "./sidebar";
+import {Outlet} from "react-router-dom";
 
 function Admin() {
   return (
-    <div className="m-2">
+    <div className="m-5 flex gap-5">
       <Sidebar />
+      <Outlet />
     </div>
   )
 }
